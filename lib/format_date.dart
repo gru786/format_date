c2fbce1a90242937507e6bd05e06c7cf65454c08
gru786 <objectsol@Objectsols-Mac-mini.local>
2023-12-27 18:26:13 +0530

    format date, format time added

--- lib/format_date.dart
+++ lib/format_date.dart
@@ -1,7 +1,88 @@
 library format_date;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class FormatDate {
+
+  static String formatTime({required DateTime dateTime, String format = "hh:mm:ss"}){
+    // 2023-12-27 15:52:04.570
+    List<String> dateAndTime = dateTime.toString().split(" ");
+    //List<String> dateInYyyyMmDd = dateAndTime[0].split("-");
+    List<String> timeTillSeconds = dateAndTime[1].split(".");
+    List<String> hhMmSs = timeTillSeconds[0].split(":");
+    if (format == "hh-mm-ss") {
+      return "${hhMmSs[0]}-${hhMmSs[1]}-${hhMmSs[2]}";
+    }
+    else if (format == "hh-mm") {
+      return "${hhMmSs[0]}-${hhMmSs[1]}";
+    } 
+    else if (format == "hh:mm:ss") {
+      return "${hhMmSs[0]}:${hhMmSs[1]}:${hhMmSs[2]}";
+    } 
+    else if (format == "hh:mm:ss.ms") {
+      return dateAndTime[1];
+    }
+    else if (format == "hh:mm") {
+      return "${hhMmSs[0]}:${hhMmSs[1]}";
+    }
+
+    return "Invalid Format";
+  }
+  static String formatDate(
+      {required DateTime dateTime, String format = "dd-mm-yyyy"}) {
+    // 2023-12-27 15:52:04.570
+    List<String> dateAndTime = dateTime.toString().split(" ");
+    
+
+    if (format == "dd-mm-yyyy") {
+      return "${dateAndTime[2]}-${dateAndTime[1]}-${dateAndTime[0]}";
+    } 
+    else if (format == "dd-mm-yy") {
+      return "${dateAndTime[2]}-${dateAndTime[1]}-${dateAndTime[0].substring( 2)}";
+    } 
+    else if (format == "mm-dd-yyyy") {
+      return "${dateAndTime[1]}-${dateAndTime[2]}-${dateAndTime[0]}";
+    }
+    else if (format == "mm-dd-yy") {
+      return "${dateAndTime[1]}-${dateAndTime[2]}-${dateAndTime[0].substring(2)}";
+    } 
+    
+    else if (format == "dd/mm/yyyy") {
+      return "${dateAndTime[2]}/${dateAndTime[1]}/${dateAndTime[0]}";
+    } 
+    else if (format == "dd/mm/yy") {
+      return "${dateAndTime[2]}/${dateAndTime[1]}/${dateAndTime[0].substring(2)}";
+    } 
+    else if (format == "mm/dd/yyyy") {
+      return "${dateAndTime[1]}/${dateAndTime[2]}/${dateAndTime[0]}";
+    }
+    else if (format == "mm/dd/yy") {
+      return "${dateAndTime[1]}/${dateAndTime[2]}/${dateAndTime[0].substring(2)}";
+    }
+
+    else if (format == "dd-mm") {
+      return "${dateAndTime[2]}-${dateAndTime[1]}";
+    } 
+    else if (format == "dd/mm") {
+      return "${dateAndTime[2]}/${dateAndTime[1]}";
+    } 
+    else if (format == "ddmm") {
+      return "${dateAndTime[2]}${dateAndTime[1]}";
+    } 
+    else if (format == "ddmmyyyy") {
+      return "${dateAndTime[2]}${dateAndTime[1]}${dateAndTime[1]}";
+    } 
+    else if (format == "yyyy") {
+      return dateAndTime[0];
+    }
+    else if (format == "yy") {
+      return dateAndTime[0].substring(2);
+    }
+    else if (format == "dd") {
+      return dateAndTime[2];
+    }
+     else if (format == "mm") {
+      return dateAndTime[1];
+    }
+
+    return "Invalid Format";
+  }
 }
